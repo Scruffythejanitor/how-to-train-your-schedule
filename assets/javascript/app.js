88664f3702622dda2dc6baa75e43115752b1ba92
Scruffythejanitor <58636685+Scruffythejanitor@users.noreply.github.com>
2020-02-11 15:49:21 -0700

    functionality of app working (MVP)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,14 @@ $(document).ready(function () {
     var nextArrival = "";
     var minutesAway = "";
 
+    function currentTime() {
+        var time = moment().format('LT')
+        $(".local-time").text(time)
+
+    }
+
+
+
     var database = {
         apiKey: "AIzaSyCTQvJ9GOKDh4mAMV0-_8z3QpDcs_FBlQM",
         authDomain: "how-to-train-your-schedule.firebaseio.com",
@@ -17,34 +25,60 @@ $(document).ready(function () {
     var database = firebase.database();
 
     $(".submit").on("click", function (event) {
-        console.log("button was clicked");
-        
+
         event.preventDefault();
 
         trainName = $("#trainName").val().trim();
         destination = $("#destination").val().trim();
         trainTime = $("#trainTime").val().trim();
         frequency = $("#frequency").val().trim();
-        
+
         database.ref().push({
             nameOfTrain: trainName,
             destOfTrain: destination,
             timeOfTrain: trainTime,
             freqOfTrain: frequency
         });
+
+        $("#trainName").val("")
+        $("#destination").val("")
+        $("#trainTime").val("")
+        $("#frequency").val("")
+
+        return false;
     });
 
     database.ref().on("child_added", function (snapshot) {
-        console.log(snapshot.val().nameOfTrain);
-        
 
+        var freqOfTrain = snapshot.child("freqOfTrain").val()
+        trainTimeConvert = moment(snapshot.val().trainTime, "hh:mm").subtract(1, "years");
+        trainDiff = moment().diff(moment(trainTimeConvert), "minutes");
+        console.log(trainDiff);
+
+        trainRemainder = trainDiff % freqOfTrain;
+        console.log(trainRemainder);
+        console.log(snapshot.child("freqOfTrain").val());
+
+        minutesAway = freqOfTrain - trainRemainder;
+        console.log(minutesAway);
+
+
+        nextArrival = moment().add(minutesAway, "m").format("hh:mm A");
+        console.log(nextArrival);
 
         $(".table-body").append(
-        "<tr><td>" + snapshot.val().nameOfTrain + "</td>"
-        + "<td>" + snapshot.val().destOfTrain + "</td>" 
-        + "<td>" + snapshot.val().freqOfTrain + "</td>"
-        // + "<td>" + snapshot.val().nameOfTrain + "</td>"
-        // + "<td>" + snapshot.val().nameOfTrain + "</td>"
+            "<tr><td>" + snapshot.val().nameOfTrain + "</td>"
+            + "<td>" + snapshot.val().destOfTrain + "</td>"
+            + "<td>" + snapshot.val().freqOfTrain + "</td>"
+            + "<td>" + nextArrival + "</td>"
+            + "<td>" + minutesAway + "</td>"
         )
     })
+
+
+
+    currentTime()
+    setInterval(currentTime, 1000)
+
+
 });
